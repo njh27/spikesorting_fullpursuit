Fixed to use threshold normalization for binary pursuit

src/consolidate.py
@@ -202,7 +202,7 @@ def compute_SNR(neurons):
     return neurons
 
 
-def remove_duplicate_spikes(event_indices, new_spike_bool):
+def remove_binary_pursuit_duplicates(event_indices, new_spike_bool):
     """
     """
     keep_bool = np.ones(event_indices.size, dtype=np.bool)
@@ -213,22 +213,18 @@ def remove_duplicate_spikes(event_indices, new_spike_bool):
             if new_spike_bool[curr_index] and ~new_spike_bool[next_index]:
                 keep_bool[curr_index] = False
                 curr_index = next_index
-                next_index += 1
             elif ~new_spike_bool[curr_index] and new_spike_bool[next_index]:
                 keep_bool[next_index] = False
-                next_index += 1
             elif new_spike_bool[curr_index] and new_spike_bool[next_index]:
                 # Should only be possible for first index?
                 keep_bool[next_index] = False
-                next_index += 1
             else:
-                # This is two spikes with same index, neither from binary
-                # pursuit. Should be chosen based on templates or some other
-                # means
-                next_index += 1
+                # Two spikes with same index, neither from binary pursuit.
+                #  Should be chosen based on templates or some other means.
+                pass
         else:
             curr_index = next_index
-            next_index += 1
+        next_index += 1
 
     return keep_bool
 
@@ -285,7 +281,7 @@ def summarize_neurons(Probe, threshold_crossings, labels, waveforms, thresholds,
                 if new_waveforms is not None:
                     neuron["new_spike_bool"] = new_wave_bool[labels[channel] == neuron_label]
                     neuron["new_spike_bool"] = neuron["new_spike_bool"][spike_order]
-                    keep_bool = remove_duplicate_spikes(neuron["spike_indices"], neuron["new_spike_bool"])
+                    keep_bool = remove_binary_pursuit_duplicates(neuron["spike_indices"], neuron["new_spike_bool"])
                     neuron["spike_indices"] = neuron["spike_indices"][keep_bool]
                     neuron["new_spike_bool"] = neuron["new_spike_bool"][keep_bool]
                     neuron['waveforms'] = neuron['waveforms'][keep_bool, :]

src/parallel/binary_pursuit_parallel.py
@@ -14,7 +14,7 @@ os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
 
 
 def binary_pursuit(probe_dict, channel, neighbors, neighbor_voltage,
-                   event_indices, neuron_labels, clip_width,
+                   event_indices, neuron_labels, clip_width, thresholds=None,
                    kernels_path=None, max_gpu_memory=None):
     """
     	binary_pursuit_opencl(voltage, crossings, labels, clips)
@@ -473,6 +473,11 @@ def binary_pursuit(probe_dict, channel, neighbors, neighbor_voltage,
     # adjusted_clips = adjusted_clips[spike_order, :]
     # Realign events with center of spike
     event_indices += clip_init_samples
+
+    if thresholds is not None:
+        for n_chan in range(0, n_neighbor_chans):
+            nt_win = [n_chan*template_samples_per_chan, n_chan*template_samples_per_chan + template_samples_per_chan]
+            adjusted_clips[:, nt_win[0]:nt_win[1]] /= thresholds[n_chan]
 
     print("Found a total of", np.count_nonzero(new_spike_bool), "secret spikes", flush=True)
 

src/parallel/spikesorting_parallel.py
@@ -393,13 +393,10 @@ def spike_sort_one_chan(data_dict, use_cpus, chan, neighbors, sigma=4.5,
                                         [crossings, neuron_labels],
                                         valid_event_indices)
 
-        # Realign spikes based on correlation with current cluster templates before doing binary pursuit
-        crossings, neuron_labels, _ = segment_parallel.align_events_with_template(data_dict, voltage[chan, :], neuron_labels, crossings, clip_width=clip_width)
-        clips, valid_event_indices = segment_parallel.get_multichannel_clips(data_dict, voltage[neighbors, :], crossings, clip_width=clip_width, neighbor_thresholds=data_dict['thresholds'][neighbors])
-        crossings, neuron_labels = segment_parallel.keep_valid_inds([crossings, neuron_labels], valid_event_indices)
-
         exit_type = "Finished sorting clusters"
+        # Realign spikes based on correlation with current cluster templates before doing binary pursuit
 
+        crossings, neuron_labels, _ = segment_parallel.align_events_with_template(data_dict, voltage[chan, :], neuron_labels, crossings, clip_width=clip_width)
         if do_binary_pursuit:
             if verbose: print("currently", np.unique(neuron_labels).size, "different clusters", flush=True)
             if verbose: print("Doing binary pursuit", flush=True)
@@ -414,10 +411,12 @@ def spike_sort_one_chan(data_dict, use_cpus, chan, neighbors, sigma=4.5,
                 crossings, neuron_labels, new_inds, clips = binary_pursuit_parallel.binary_pursuit(
                             data_dict, chan, neighbors, voltage[neighbors, :],
                             crossings, neuron_labels, clip_width,
-                            kernels_path=None,
-                            max_gpu_memory=max_gpu_memory)
+                            thresholds=data_dict['thresholds'][neighbors],
+                            kernels_path=None, max_gpu_memory=max_gpu_memory)
             exit_type = "Finished binary pursuit"
         else:
+            clips, valid_event_indices = segment_parallel.get_multichannel_clips(data_dict, voltage[neighbors, :], crossings, clip_width=clip_width, neighbor_thresholds=data_dict['thresholds'][neighbors])
+            crossings, neuron_labels = segment_parallel.keep_valid_inds([crossings, neuron_labels], valid_event_indices)
             new_inds = np.zeros(crossings.size, dtype='bool')
 
         if verbose: print("currently", np.unique(neuron_labels).size, "different clusters", flush=True)

src/spikesorting.py
@@ -216,9 +216,6 @@ def spike_sort(Probe, sigma=4.5, clip_width=[-6e-4, 10e-4],
 
         # Realign spikes based on correlation with current cluster templates before doing binary pursuit
         crossings[chan], neuron_labels, _ = segment.align_events_with_template(Probe, chan, neuron_labels, crossings[chan], clip_width=clip_width)
-        clips, valid_event_indices = segment.get_multichannel_clips(Probe, Probe.get_neighbors(chan), crossings[chan], clip_width=clip_width, thresholds=thresholds)
-        crossings[chan], neuron_labels = segment.keep_valid_inds([crossings[chan], neuron_labels], valid_event_indices)
-
         if do_binary_pursuit:
             if verbose: print("currently", np.unique(neuron_labels).size, "different clusters")
             if verbose: print("Doing binary pursuit")
@@ -230,8 +227,12 @@ def spike_sort(Probe, sigma=4.5, clip_width=[-6e-4, 10e-4],
             # crossings[chan], neuron_labels = segment.keep_valid_inds([crossings[chan], neuron_labels], valid_event_indices)
             crossings[chan], neuron_labels, new_inds, clips = binary_pursuit.binary_pursuit(
                 Probe, chan, crossings[chan], neuron_labels, clip_width,
-                kernels_path=None, max_gpu_memory=max_gpu_memory)
+                thresholds=thresholds, kernels_path=None,
+                max_gpu_memory=max_gpu_memory)
         else:
+            # Need to get newly aligned clips and new_inds = False
+            clips, valid_event_indices = segment.get_multichannel_clips(Probe, Probe.get_neighbors(chan), crossings[chan], clip_width=clip_width, thresholds=thresholds)
+            crossings[chan], neuron_labels = segment.keep_valid_inds([crossings[chan], neuron_labels], valid_event_indices)
             new_inds = np.zeros(crossings[chan].size, dtype=np.bool)
 
         if verbose: print("currently", np.unique(neuron_labels).size, "different clusters")
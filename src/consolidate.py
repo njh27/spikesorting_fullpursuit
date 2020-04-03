Fixed bug with PC computation when clips are all equal or less than 2 clips

src/consolidate.py
@@ -531,7 +531,7 @@ class WorkItemSummary(object):
                     clips_2 = self.sort_data[chan][next_seg][2][fake_select, :]
                     is_merged, _, _ = self.merge_test_two_units(
                             clips_1, clips_2, self.sort_info['p_value_cut_thresh'],
-                            method='projection', merge_only=True)
+                            method='template_pca', merge_only=True)
                     if is_merged:
                         # Update actual next segment label data with same labels
                         # used in curr_seg
@@ -576,7 +576,7 @@ class WorkItemSummary(object):
                     clips_2 = joint_clips[c2_select, :]
                     ismerged, labels_1, labels_2 = self.merge_test_two_units(
                             clips_1, clips_2, self.sort_info['p_value_cut_thresh'],
-                            method='projection', split_only=True)
+                            method='template_pca', split_only=True)
                     if ismerged: # This can happen if the split cutpoint forces
                         continue # a merge so check and skip
                     if 2 in labels_1:
@@ -665,6 +665,53 @@ class WorkItemSummary(object):
                     if mua_ratio > self.max_mua_ratio:
                         self.delete_label(chan, curr_seg, curr_l)
 
+    def summarize_neurons_by_seg(self):
+        """
+        """
+        self.neuron_summary_by_seg = [[] for x in range(0, self.n_chans)]
+        for chan in range(0, self.n_chans):
+            for seg in range(0, len(self.sort_data[chan])):
+                self.neuron_summary_by_seg[chan].append([])
+                for ind, neuron_label in enumerate(np.unique(self.sort_data[chan][seg][1])):
+                    neuron = {}
+                    select_label = self.sort_data[chan][seg][1] == neuron_label
+                    neuron["spike_indices"] = self.sort_data[chan][seg][0][select_label]
+                    neuron['waveforms'] = self.sort_data[chan][seg][2][select_label, :]
+                    neuron["new_spike_bool"] = self.sort_data[chan][seg][3][select_label]
+
+                    # NOTE: This still needs to be done even though segments
+                    # were ordered because of overlap!
+                    # Ensure spike times are ordered. Must use 'stable' sort for
+                    # output to be repeatable because overlapping segments and
+                    # binary pursuit can return slightly different dupliate spikes
+                    spike_order = np.argsort(neuron["spike_indices"], kind='stable')
+                    neuron["spike_indices"] = neuron["spike_indices"][spike_order]
+                    neuron['waveforms'] = neuron['waveforms'][spike_order, :]
+                    neuron["new_spike_bool"] = neuron["new_spike_bool"][spike_order]
+                    # Remove duplicates found in binary pursuit
+                    keep_bool = remove_binary_pursuit_duplicates(neuron["spike_indices"],
+                                    neuron["new_spike_bool"],
+                                    tol_inds=self.duplicate_tol_inds)
+                    neuron["spike_indices"] = neuron["spike_indices"][keep_bool]
+                    neuron["new_spike_bool"] = neuron["new_spike_bool"][keep_bool]
+                    neuron['waveforms'] = neuron['waveforms'][keep_bool, :]
+
+                    # Remove any identical index duplicates (either from error or
+                    # from combining overlapping segments), preferentially keeping
+                    # the waveform best aligned to the template
+                    neuron["template"] = np.mean(neuron['waveforms'], axis=0)
+                    keep_bool = remove_spike_event_duplicates(neuron["spike_indices"],
+                                    neuron['waveforms'], neuron["template"],
+                                    tol_inds=self.duplicate_tol_inds)
+                    neuron["spike_indices"] = neuron["spike_indices"][keep_bool]
+                    neuron["new_spike_bool"] = neuron["new_spike_bool"][keep_bool]
+                    neuron['waveforms'] = neuron['waveforms'][keep_bool, :]
+
+                    # Recompute template and store output
+                    neuron["template"] = np.mean(neuron['waveforms'], axis=0)
+                    neuron["snr"] = self.snr_norm(neuron["template"])
+                    self.neuron_summary_by_seg[chan][seg].append(neuron)
+
     def get_sort_data_by_chan(self):
         """ Returns all sorter data concatenated by channel across all segments,
         thus removing the concept of segment-wise sorting and giving all data
@@ -713,6 +760,7 @@ class WorkItemSummary(object):
         """
         # To the sort data segments into our conventional sorter items by
         # channel independent of segment/work item
+        print("THIS SHOUD RETURN AN SNR FOR EVERY SEGEMENT OF A GIVEN NEURON. THIS COULD BE VERY USEFUL LATER IF COMBINING OVER CHANNELS...")
         crossings, labels, waveforms, new_waveforms = self.get_sort_data_by_chan()
         neuron_summary = []
         for channel in range(0, len(crossings)):

src/parallel/spikesorting_parallel.py
@@ -269,7 +269,7 @@ def branch_pca_2_0(neuron_labels, clips, curr_chan_inds, p_value_cut_thresh=0.01
         clust_clips = clips[curr_clust_bool, :]
         if clust_clips.ndim == 1:
             clust_clips = np.expand_dims(clust_clips, 0)
-        if clust_clips.shape[0] == 1:
+        if clust_clips.shape[0] <= 1:
             # Only one spike so don't try to sort
             continue
         median_cluster_size = min(100, int(np.around(clust_clips.shape[0] / 1000)))
@@ -693,7 +693,7 @@ def spike_sort_parallel(Probe, **kwargs):
         n_complete = len(data_dict['completed_items']) # Do once to avoid race
         if n_complete > completed_items_index:
             for ci in range(completed_items_index, n_complete):
-                print("Completed item", work_items[data_dict['completed_items'][ci]]['ID'], "from chan", work_items[data_dict['completed_items'][ci]]['channel'], "segment", work_items[data_dict['completed_items'][ci]]['seg_number']+1)
+                print("Completed item", work_items[data_dict['completed_items'][ci]]['ID']+1, "from chan", work_items[data_dict['completed_items'][ci]]['channel'], "segment", work_items[data_dict['completed_items'][ci]]['seg_number'])
                 print("Exited with status: ", data_dict['exits_dict'][data_dict['completed_items'][ci]])
                 completed_items_index += 1
                 if not settings['test_flag']:
@@ -728,7 +728,7 @@ def spike_sort_parallel(Probe, **kwargs):
                 # This item was already finished above so just clearing out
                 # completed_items_queue
                 continue
-            print("Completed item", finished_item, "from chan", work_items[finished_item]['channel'], "segment", work_items[finished_item]['seg_number']+1)
+            print("Completed item", finished_item+1, "from chan", work_items[finished_item]['channel'], "segment", work_items[finished_item]['seg_number'])
             print("Exited with status: ", data_dict['exits_dict'][finished_item])
             completed_items_index += 1
 

src/preprocessing.py
@@ -90,7 +90,7 @@ def pca_scores(spikes, compute_pcs=None, pcs_as_index=True, return_V=False, retu
 
     if spikes.ndim != 2:
         raise ValueError("Input 'spikes' must be a 2 dimensional array to compute PCA")
-    if spikes.shape[0] == 1:
+    if spikes.shape[0] <= 1:
         raise ValueError("Must input more than 1 spike to compute PCA")
     if compute_pcs is None:
         compute_pcs = spikes.shape[1]
@@ -113,12 +113,10 @@ def pca_scores(spikes, compute_pcs=None, pcs_as_index=True, return_V=False, retu
         E, V = E[key], V[:, key]
         U = np.matmul(spike_copy, V)
     else:
-        # No variance, all PCs are equal! Set to 0
-        if compute_pcs is None:
-            compute_pcs = spikes.shape[1]
-        U = np.array(np.nan) #np.zeros((spikes.shape[0], compute_pcs))
-        V = np.array(np.nan) #np.zeros((spikes.shape[1], compute_pcs))
-        E = np.array(np.nan) #np.ones(compute_pcs)
+        # No variance, all PCs are equal! Set to None(s)
+        U = None
+        V = None
+        E = None
 
     if return_V and return_E:
         return U, V, E
@@ -148,13 +146,13 @@ def optimal_reconstruction_pca_order(spikes, check_components=None,
         check_components = spikes.shape[1]
     max_components = np.amin([max_components, spikes.shape[1]])
     check_components = np.amin([check_components, spikes.shape[1]])
-    if (max_components <= 1) or (check_components <= 1):
-        # Only choosing from among the first PC so just return index to first PC
-        return np.array([0])
 
     # Get residual sum of squared error for each PC separately
     resid_error = np.zeros(check_components)
     _, components = pca_scores(spikes, check_components, pcs_as_index=False, return_V=True)
+    if components is None:
+        # Couldn't compute PCs
+        return np.zeros(1, dtype=np.int64), True
     for comp in range(0, check_components):
         reconstruction = (spikes @ components[:, comp][:, None]) @ components[:, comp][:, None].T
         RESS = np.mean(np.mean((reconstruction - spikes) ** 2, axis=1), axis=0)
@@ -224,6 +222,10 @@ def compute_pca(clips, check_components, max_components, add_peak_valley=False,
         use_components, _ = sort_cython.optimal_reconstruction_pca_order_F(clips, check_components, max_components)
     # print("Automatic component detection chose", use_components, "PCA components.", flush=True)
     scores = pca_scores(clips, use_components, pcs_as_index=True)
+    if scores is None:
+        # Usually means all clips were the same or there was <= 1, so PCs can't
+        # be computed. Just return everything as the same score
+        return np.zeros((clips.shape[0], 1))
     if add_peak_valley:
         peak_valley = (np.amax(clips[:, curr_chan_inds], axis=1) - np.amin(clips[:, curr_chan_inds], axis=1)).reshape(clips.shape[0], -1)
         peak_valley /= np.amax(np.abs(peak_valley)) # Normalized from -1 to 1
@@ -240,9 +242,16 @@ def compute_pca_by_channel(clips, curr_chan_inds, check_components,
     pcs_by_chan = []
     # Do current channel first
     # use_components, _ = optimal_reconstruction_pca_order(clips[:, curr_chan_inds], check_components, max_components, min_components=0)
-    use_components, _ = sort_cython.optimal_reconstruction_pca_order_F(clips[:, curr_chan_inds], check_components, max_components, min_components=0)
+    if clips.flags['C_CONTIGUOUS']:
+        use_components, _ = sort_cython.optimal_reconstruction_pca_order(clips[:, curr_chan_inds], check_components, max_components)
+    else:
+        use_components, _ = sort_cython.optimal_reconstruction_pca_order_F(clips[:, curr_chan_inds], check_components, max_components)
     # print("Automatic component detection (get by channel) chose", use_components, "PCA components.", flush=True)
     scores = pca_scores(clips[:, curr_chan_inds], use_components, pcs_as_index=True)
+    if scores is None:
+        # Usually means all clips were the same or there was <= 1, so PCs can't
+        # be computed. Just return everything as the same score
+        return np.zeros((clips.shape[0], 1))
     if add_peak_valley:
         peak_valley = (np.amax(clips[:, curr_chan_inds], axis=1) - np.amin(clips[:, curr_chan_inds], axis=1)).reshape(clips.shape[0], -1)
         peak_valley /= np.amax(np.abs(peak_valley)) # Normalized from -1 to 1
@@ -258,7 +267,10 @@ def compute_pca_by_channel(clips, curr_chan_inds, check_components,
             continue
         ch_inds = np.arange(ch*samples_per_chan, (ch+1)*samples_per_chan)
         # use_components, is_worse_than_mean = optimal_reconstruction_pca_order(clips[:, ch_inds], check_components, max_components)
-        use_components, is_worse_than_mean = sort_cython.optimal_reconstruction_pca_order_F(clips[:, ch_inds], check_components, max_components)
+        if clips.flags['C_CONTIGUOUS']:
+            use_components, is_worse_than_mean = sort_cython.optimal_reconstruction_pca_order(clips[:, ch_inds], check_components, max_components)
+        else:
+            use_components, is_worse_than_mean = sort_cython.optimal_reconstruction_pca_order_F(clips[:, ch_inds], check_components, max_components)
         if is_worse_than_mean:
             # print("Automatic component detection (get by channel) chose !NO! PCA components.", flush=True)
             continue
@@ -375,9 +387,16 @@ def compute_template_pca(clips, labels, curr_chan_inds, check_components, max_co
         templates[ind, :] = np.mean(clips[labels == l, :], axis=0) * np.sqrt(u_counts[ind] / labels.size)
 
     # use_components, _ = optimal_reconstruction_pca_order(templates, check_components, max_components)
-    use_components, _ = sort_cython.optimal_reconstruction_pca_order(templates, check_components, max_components)
+    if templates.flags['C_CONTIGUOUS']:
+        use_components, _ = sort_cython.optimal_reconstruction_pca_order(templates, check_components, max_components)
+    else:
+        use_components, _ = sort_cython.optimal_reconstruction_pca_order_F(templates, check_components, max_components)
     # print("Automatic component detection (FULL TEMPLATES) chose", use_components, "PCA components.")
     _, score_mat = pca_scores(templates, use_components, pcs_as_index=True, return_V=True)
+    if score_mat is None:
+        # Usually means all clips were the same or there was <= 1, so PCs can't
+        # be computed. Just return everything as the same score
+        return np.zeros((clips.shape[0], 1))
     scores = clips @ score_mat
     if add_peak_valley:
         peak_valley = (np.amax(clips[:, curr_chan_inds], axis=1) - np.amin(clips[:, curr_chan_inds], axis=1)).reshape(clips.shape[0], -1)
@@ -403,9 +422,16 @@ def compute_template_pca_by_channel(clips, labels, curr_chan_inds, check_compone
     pcs_by_chan = []
     # Do current channel first
     # use_components, _ = optimal_reconstruction_pca_order(templates[:, curr_chan_inds], check_components, max_components)
-    use_components, _ = sort_cython.optimal_reconstruction_pca_order_F(templates[:, curr_chan_inds], check_components, max_components)
+    if templates.flags['C_CONTIGUOUS']:
+        use_components, _ = sort_cython.optimal_reconstruction_pca_order(templates[:, curr_chan_inds], check_components, max_components)
+    else:
+        use_components, _ = sort_cython.optimal_reconstruction_pca_order_F(templates[:, curr_chan_inds], check_components, max_components)
     # print("Automatic component detection (TEMPLATES by channel) chose", use_components, "PCA components.")
     _, score_mat = pca_scores(templates[:, curr_chan_inds], use_components, pcs_as_index=True, return_V=True)
+    if score_mat is None:
+        # Usually means all clips were the same or there was <= 1, so PCs can't
+        # be computed. Just return everything as the same score
+        return np.zeros((clips.shape[0], 1))
     scores = clips[:, curr_chan_inds] @ score_mat
     if add_peak_valley:
         peak_valley = (np.amax(clips[:, curr_chan_inds], axis=1) - np.amin(clips[:, curr_chan_inds], axis=1)).reshape(clips.shape[0], -1)
@@ -421,7 +447,10 @@ def compute_template_pca_by_channel(clips, labels, curr_chan_inds, check_compone
             continue
         ch_inds = np.arange(ch*samples_per_chan, (ch+1)*samples_per_chan)
         # use_components, is_worse_than_mean = optimal_reconstruction_pca_order(templates[:, ch_inds], check_components, max_components)
-        use_components, is_worse_than_mean = sort_cython.optimal_reconstruction_pca_order_F(templates[:, ch_inds], check_components, max_components)
+        if templates.flags['C_CONTIGUOUS']:
+            use_components, is_worse_than_mean = sort_cython.optimal_reconstruction_pca_order(templates[:, ch_inds], check_components, max_components)
+        else:
+            use_components, is_worse_than_mean = sort_cython.optimal_reconstruction_pca_order_F(templates[:, ch_inds], check_components, max_components)
         if is_worse_than_mean:
             # print("Automatic component detection (TEMPLATES by channel) chose !NO! PCA components.", flush=True)
             continue

src/sort.py
@@ -45,7 +45,7 @@ def initial_cluster_farthest(data, median_cluster_size, choose_percentile=0.95, 
     """
     if data.ndim <= 1 or data.size == 1:
         # Only 1 spike so return 1 label!
-        return np.array([0], dtype=np.int64)
+        return np.zeros(1, dtype=np.int64)
 
     # Begin with a single cluster (all data belong to the same cluster)
     labels = np.zeros((data.shape[0]), dtype=np.int64)
@@ -58,6 +58,9 @@ def initial_cluster_farthest(data, median_cluster_size, choose_percentile=0.95, 
         return labels
     centers = np.mean(data, axis=0)
     distances = np.sum((data - centers)**2, axis=1)
+    if np.all(np.all(distances == 0, axis=1), axis=0):
+        # All scores are the same, so return all same label
+        return labels
 
     if n_random > 0:
         if n_random >= labels.size:

src/spikesorting.py
@@ -81,7 +81,7 @@ def branch_pca_2_0(neuron_labels, clips, curr_chan_inds, p_value_cut_thresh=0.01
         clust_clips = clips[curr_clust_bool, :]
         if clust_clips.ndim == 1:
             clust_clips = np.expand_dims(clust_clips, 0)
-        if clust_clips.shape[0] == 1:
+        if clust_clips.shape[0] <= 1:
             # Only one spike so don't try to sort
             continue
         median_cluster_size = min(100, int(np.around(clust_clips.shape[0] / 1000)))
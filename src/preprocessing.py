5b0e3920b44a4d4836120a10d878251eae3ef104
njh27 <njh27@duke.edu>
2020-04-20 07:41:08 -0400

    zca threshold

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -39,6 +39,7 @@ def get_noise_sampled_zca_matrix(voltage_data, thresholds, sigma, thresh_cushion
         # Since binary pursuit can dip roughly halfway below threshold, we can
         # adjust our noise computation accordingly
         # zca_thresholds /= 2
+        # zca_thresholds[:] = np.inf
         pass
     # convert cushion to zero centered window
     thresh_cushion = (thresh_cushion * 2 + 1)

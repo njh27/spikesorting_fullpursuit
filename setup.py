9703e095296ea117f573ba43882f481310d0874e
njh27 <njh27@duke.edu>
2020-06-04 12:03:46 -0400

    added setup to track

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy as np
+
+ext  =  [Extension('src.c_cython.sort_cython', sources=['src/c_cython/sort_cython.pyx'])]
+pkg_req = ['numpy', 'Cython']
+
+setup(name='SpikeSorting',
+      version='1.0',
+      description='Spike sorting algorithm with overlap detection.',
+      author='Nathan Hall',
+      author_email='nathan.hall@duke.edu',
+      url='https://',
+      packages=['src', 'test'],
+      ext_modules=cythonize(ext, annotate=False, language_level=3),
+      include_dirs=[np.get_include()],
+      requires=pkg_req
+     )

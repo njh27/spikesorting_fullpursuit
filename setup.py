fed9e106bb50eb517df7bddbb57c918405b8075d
njh27 <njh27@duke.edu>
2020-06-12 13:04:26 -0400

    initial setup

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 
-ext  =  [Extension('so_sorting.src.c_cython.sort_cython', sources=['so_sorting/src/c_cython/sort_cython.pyx'])]
+ext  =  [Extension('fbp.src.c_cython.sort_cython', sources=['fbp/src/c_cython/sort_cython.pyx'])]
 pkg_req = ['numpy', 'Cython']
 
-setup(name='SOSorting',
+setup(name='Full Binary Pursuit',
       version='1.0',
       description='Spike sorting algorithm with overlap detection.',
       author='Nathan Hall',
       author_email='nathan.hall@duke.edu',
       url='https://',
-      packages=['so_sorting'],
+      packages=['fbp'],
       ext_modules=cythonize(ext, annotate=False, language_level=3),
       include_dirs=[np.get_include()],
       requires=pkg_req

fixed bug in SNR computation for midway summary

fbp/src/consolidate.py
@@ -702,10 +702,10 @@ class SegSummary(object):
                 neuron['summary_type'] = 'single_segment'
                 neuron["channel"] = self.work_items[n_wi]['channel']
                 neuron['neighbors'] = self.work_items[n_wi]['neighbors']
-                neuron['chan_neighbor_ind'] = self.work_items[n_wi]['chan_neighbor_ind']
-                neuron['main_win'] = [self.sort_info['n_samples_per_chan'] * neuron['chan_neighbor_ind'],
-                                      self.sort_info['n_samples_per_chan'] * (neuron['chan_neighbor_ind'] + 1)]
-                neuron['threshold'] = self.work_items[n_wi]['thresholds'][neuron['chan_neighbor_ind']]
+                # This assumes that input has all channels in order!
+                neuron['main_win'] = [self.sort_info['n_samples_per_chan'] * neuron["channel"],
+                                      self.sort_info['n_samples_per_chan'] * (neuron["channel"] + 1)]
+                neuron['threshold'] = self.work_items[n_wi]['thresholds'][neuron['channel']]
 
                 select_label = self.sort_data[n_wi][1] == neuron_label
                 neuron["spike_indices"] = self.sort_data[n_wi][0][select_label]
@@ -738,6 +738,11 @@ class SegSummary(object):
                 # Recompute template and store output
                 neuron["template"] = np.mean(neuron['clips'], axis=0).astype(neuron['clips'].dtype)
                 neuron['snr'] = self.get_snr(neuron)
+                print("NEURON SNR IS", neuron['snr'], "channel", neuron['channel'])
+                if neuron['snr'] < 1.:
+                    # SNR this low indicates true garbage that will only slow
+                    # binary pursuit so skip it outright
+                    continue
                 neuron['fraction_mua'] = calc_fraction_mua_to_peak(
                                             neuron["spike_indices"],
                                             self.sort_info['sampling_rate'],
@@ -890,7 +895,6 @@ class SegSummary(object):
             return best_pair, best_shift, clips_1, clips_2
         else:
             # This is probably not a good match afterall, so try again
-            print("NEAREST SHIFTED PAIR IS RECURSING")
             previously_compared_pairs.append(best_pair)
             best_pair, best_shift, clips_1, clips_2 = self.find_nearest_shifted_pair(remaining_inds, previously_compared_pairs)
             return best_pair, best_shift, clips_1, clips_2
@@ -903,24 +907,24 @@ class SegSummary(object):
         clips = np.vstack((clips_1, clips_2))
         orig_neuron_labels = np.ones(clips.shape[0], dtype=np.int64)
         orig_neuron_labels[clips_1.shape[0]:] = 2
-        scores = preprocessing.compute_template_pca(clips, orig_neuron_labels,
-                    curr_chan_inds, self.sort_info['check_components'],
-                    self.sort_info['max_components'],
-                    add_peak_valley=self.sort_info['add_peak_valley'],
-                    use_weights=True)
+        # scores = preprocessing.compute_template_pca(clips, orig_neuron_labels,
+        #             curr_chan_inds, self.sort_info['check_components'],
+        #             self.sort_info['max_components'],
+        #             add_peak_valley=self.sort_info['add_peak_valley'],
+        #             use_weights=True)
         # scores = preprocessing.compute_pca(clips,
         #             self.sort_info['check_components'],
         #             self.sort_info['max_components'],
         #             add_peak_valley=self.sort_info['add_peak_valley'],
         #             curr_chan_inds=curr_chan_inds)
 
-        # # Projection onto templates, weighted by number of spikes
-        # t1 = np.mean(clips_1, axis=0)
-        # t2 = np.mean(clips_2, axis=0)
-        # if use_weights:
-        #     t1 *= (clips_1.shape[0] / clips.shape[0])
-        #     t2 *= (clips_2.shape[0] / clips.shape[0])
-        # scores = clips @ np.vstack((t1, t2)).T
+        # Projection onto templates, weighted by number of spikes
+        t1 = np.mean(clips_1, axis=0)
+        t2 = np.mean(clips_2, axis=0)
+        if use_weights:
+            t1 *= (clips_1.shape[0] / clips.shape[0])
+            t2 *= (clips_2.shape[0] / clips.shape[0])
+        scores = clips @ np.vstack((t1, t2)).T
 
         scores = np.float64(scores)
 

fbp/src/parallel/binary_pursuit_parallel.py
@@ -273,7 +273,7 @@ def binary_pursuit(templates, voltage, template_labels, sampling_rate, v_dtype,
         # Loop over chunks
         for chunk_number, start_index in enumerate(chunk_onsets):
             stop_index = np.uint32(min(n_samples, start_index + num_indices_per_chunk))
-            print("Starting chunk number", chunk_number, "from", start_index, "to", stop_index, flush=True)
+            print("Starting chunk number", chunk_number, "from", start_index, "to", stop_index, "samples", flush=True)
             chunk_voltage = np.float32(voltage[:, start_index:stop_index])
             chunk_voltage_length = np.uint32(stop_index - start_index)
             # Reshape voltage over channels into a single 1D vector
@@ -368,6 +368,7 @@ def binary_pursuit(templates, voltage, template_labels, sampling_rate, v_dtype,
                 n_loops += 1
                 if n_loops % 10 == 0:
                     print("Starting loop", n_loops, "for this chunk")
+                    print("Next round has", np.count_nonzero(next_check_window), "windows to check")
                 n_to_enqueue = min(total_work_size_pursuit, max_enqueue_pursuit)
                 for template_index in range(0, templates.shape[0]):
                     for enqueue_step in np.arange(0, total_work_size_pursuit, max_enqueue_pursuit, dtype=np.uint32):
@@ -394,13 +395,12 @@ def binary_pursuit(templates, voltage, template_labels, sampling_rate, v_dtype,
 
                 cl.enqueue_copy(queue, num_additional_spikes, num_additional_spikes_buffer, wait_for=next_wait_event)
                 # print("Added", num_additional_spikes[0], "secret spikes", flush=True)
-                # Read out the data from next_check_window_buffer
-                # Already waited for pursuit to finish above
-                next_wait_event = [cl.enqueue_copy(queue, next_check_window, next_check_window_buffer, wait_for=None)]
-                print("Next round has", np.count_nonzero(next_check_window))
                 if (num_additional_spikes[0] == 0):
                     break # Converged, no spikes added in last pass
 
+                # Read out the data from next_check_window_buffer
+                # Already waited for pursuit to finish above
+                next_wait_event = [cl.enqueue_copy(queue, next_check_window, next_check_window_buffer, wait_for=None)]
                 # Use next_check_window data to determine window indices for next pass
                 new_window_indices = np.uint32(np.nonzero(next_check_window)[0])
                 # Copy the new window indices to the window indices buffer

fbp/src/parallel/spikesorting_parallel.py
@@ -35,7 +35,7 @@ def spike_sorting_settings_parallel(**kwargs):
     settings['add_peak_valley'] = False # Use peak valley in addition to PCs for sorting
     settings['check_components'] = None # Number of PCs to check. None means all
     settings['max_components'] = 10 # Max number to use, of those checked
-    settings['min_firing_rate'] = 1. # Neurons with fewer threshold crossings than this are removed
+    settings['min_firing_rate'] = 0. # Neurons with fewer threshold crossings than this are removed
     settings['p_value_cut_thresh'] = 0.05
     settings['max_gpu_memory'] = None # Use as much memory as possible
     settings['save_1_cpu'] = True
@@ -319,7 +319,6 @@ def spike_sort_item_parallel(data_dict, use_cpus, work_item, settings):
         keep_clips = preprocessing.keep_max_on_main(clips, curr_chan_inds)
         clips = clips[keep_clips, :]
         crossings = crossings[keep_clips]
-
         curr_num_clusters, n_per_cluster = np.unique(neuron_labels, return_counts=True)
         if settings['verbose']: print("After keep max on main removed", np.count_nonzero(~keep_clips), "clips", flush=True)
 
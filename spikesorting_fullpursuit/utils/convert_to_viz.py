10aa9e25d2111c557c813bad94a03adf53f2568a
njh27 <njh27@duke.edu>
2021-12-08 11:12:11 -0500

    small change to convert_to_viz

--- spikesorting_fullpursuit/utils/convert_to_viz.py
+++ spikesorting_fullpursuit/utils/convert_to_viz.py
@@ -3,12 +3,17 @@ from spikesorting_fullpursuit.utils.format import to_neuroviz
 
 
 
+"""
+    convert_to_viz.py fname_neurons
+"""
+
 if __name__ == '__main__':
 
 
     fname_neurons = sys.argv[1]
+    fname_neurons = fname_neurons.rsplit(".")
     if fname_neurons[-4:] != ".pkl" and fname_neurons[-7:] != ".pickle":
-        fname_neurons = fname_neurons + ".pickle"
+        fname_neurons = fname_neurons + ".pkl"
     if fname_neurons[0:8] == "neurons_":
         save_fname = fname_neurons.split("neurons_")[1]
     save_fname = save_fname.split(".")[0]

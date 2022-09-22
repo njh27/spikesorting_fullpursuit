7fb70ce548a7ad722b8c57b2d1955b0a97d43f6a
Nate <njh27@duke.edu>
2022-09-22 11:33:14 -0400

    updated neuroviz conversion with exportable function

--- spikesorting_fullpursuit/utils/convert_to_viz.py
+++ spikesorting_fullpursuit/utils/convert_to_viz.py
@@ -3,14 +3,7 @@ from spikesorting_fullpursuit.utils.format import to_neuroviz
 
 
 
-"""
-    convert_to_viz.py fname_neurons [...save_fname, ]
-"""
-""" /volumes/t7/spikesortingfiles/bin_files/learndirtunepurk_dandy_06_volt.bin """
-if __name__ == '__main__':
-
-
-    fname_neurons = sys.argv[1]
+def f_neurons_to_viz(fname_neurons, save_fname=None, neuroviz_only=False, filename=None):
     fname_neurons = fname_neurons.rsplit(".")
     if len(fname_neurons) == 2:
         if fname_neurons[1] != "pkl" and fname_neurons[1] != "pickle":
@@ -21,9 +14,25 @@ if __name__ == '__main__':
         fname_neurons = fname_neurons + ".pkl"
     else:
         raise ValueError("Cannot parse multiple file extensions on input neurons filename")
-    if len(sys.argv) == 2:
+    if save_fname is None:
         save_fname = fname_neurons.split(".")[0]
         save_fname = save_fname + "_viz.pkl"
+
+    to_neuroviz(fname_neurons, save_fname, neuroviz_only=False, filename=None)
+
+    return None
+
+
+"""
+    convert_to_viz.py fname_neurons [...save_fname, ]
+"""
+""" /volumes/t7/spikesortingfiles/bin_files/learndirtunepurk_dandy_06_volt.bin """
+if __name__ == '__main__':
+
+
+    fname_neurons = sys.argv[1]
+    if len(sys.argv) == 2:
+        save_fname = None
     elif len(sys.argv) >= 3:
         save_fname = sys.argv[2]
         if save_fname[-4:] == ".pkl" or save_fname[-7:] == ".pickle":
@@ -32,4 +41,5 @@ if __name__ == '__main__':
             save_fname = save_fname + ".pkl"
         else:
             print("Save file name does not indicate _viz file or .pkl.")
-    to_neuroviz(fname_neurons, save_fname, neuroviz_only=False, filename=None)
+
+    f_neurons_to_viz(fname_neurons, save_fname=save_fname, neuroviz_only=False, filename=None)
